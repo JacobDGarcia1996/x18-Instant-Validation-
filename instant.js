e41eb8fe930a0526398a426dc997f4a2bb0cd1ca
Jacob Garcia <jgarcia224@mail.csuchico.edu>
2017-05-05 14:48:36 -0700

    changes made but now it doesnt want to work it may be due to the placment of the add and remove sequences

--- instant.js
+++ instant.js
@@ -1,51 +1,94 @@
 function register(){
   //Grab the user's input and store in variables
-  var userEntered = document.getElementById("user").oninput=validateUsername();
-  var passEntered = document.getElementById("pass").oninput=validatePassword();
-}
-function ValidateUsername(){
 
-  if (userEntered.length>=6)
-  {
-  document.getElementById("usernameError").innerHTML="Username Acceptable.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-success");
-document.getElementById("usernameGroup").classList.remove("has-error");
-}
 
-else
-{//Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error")
-  document.getElementById("usernameGroup").classList.remove("has-success");
-}
-}
 
-function ValidatePassword () {
-  if (passEntered =="password")
-  {//Show message that there is an error with the password...
+  var userEntered = document.getElementById("user").oninput = "validateUsername()";
+  var passEntered = document.getElementById("pass").oninput= "validatePassword()";
+function validateUsername(){
+  if (userEntered.length>=5){
+    document.getElementById("usernameError").innerHTML="Username Acceptable.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.remove("has-error");
+    document.getElementById("usernameGroup").classList.add("has-success");}
+
+  else {//Show message that there is an error with the username...
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("passwordGroup").classList.remove("has-success");
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+  if (passEntered.toLowerCase()=="password"){
+    //Show message that there is an error with the password...
     document.getElementById("passwordError").innerHTML="Bad password.";
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
     //Turn the password items red
-    document.getElementById("passwordGroup").classList.add("has-error")
     document.getElementById("passwordGroup").classList.remove("has-success");
+    document.getElementById("passwordGroup").classList.add("has-error");
   }
-
-  else
-  {
+  else {
   document.getElementById("passwordError").innerHTML="Password Approved.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-success")
-  document.getElementById("passwordGroup").classList.remove("has-error");
+  document.getElementById("usernameGroup").classList.remove("has-error");
+  document.getElementById("passwordGroup").classList.add("has-success");
   }
 }
-
+function validatePassword(){
+  if (passEntered.length<=0){
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.remove("has-success");
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else {
+  document.getElementById("passwordError").innerHTML="Password Approved.";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.remove("has-error");
+  document.getElementById("passwordGroup").classList.add("has-success");
+  }
+  if (passEntered.length>=10){
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.remove("has-success");
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else {
+  document.getElementById("passwordError").innerHTML="Password Approved.";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.remove("has-error");
+  document.getElementById("passwordGroup").classList.add("has-success");
+  }
+  if (passEntered==userEntered){
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.remove("has-success");
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else {
+  document.getElementById("passwordError").innerHTML="Password Approved.";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.remove("has-error");
+  document.getElementById("passwordGroup").classList.add("has-success");
+}
+}
 }
