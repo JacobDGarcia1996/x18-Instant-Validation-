e84e99879f8271879ed6c44408378356a36c3694
Jacob Garcia <jgarcia224@mail.csuchico.edu>
2017-05-05 21:43:16 -0700

    still not responding trying something else

--- instant.js
+++ instant.js
@@ -11,16 +11,16 @@ function validateUsername(){
     document.getElementById("usernameError").classList.remove("hidden-message");
     document.getElementById("usernameError").classList.add("shown-message");
     //Turn the username items red
-    document.getElementById("usernameGroup").classList.remove("has-error");
+
     document.getElementById("usernameGroup").classList.add("has-success");}
 
   else {//Show message that there is an error with the username...
     document.getElementById("usernameError").innerHTML="Bad username.";
-    document.getElementById("passwordGroup").classList.remove("has-success");
+
     document.getElementById("usernameError").classList.remove("hidden-message");
     document.getElementById("usernameError").classList.add("shown-message");
     //Turn the username items red
-    
+
     document.getElementById("usernameGroup").classList.add("has-error");
   }
   if (passEntered.toLowerCase()=="password"){
@@ -29,7 +29,7 @@ function validateUsername(){
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
     //Turn the password items red
-    document.getElementById("passwordGroup").classList.remove("has-success");
+
     document.getElementById("passwordGroup").classList.add("has-error");
   }
   else {
@@ -37,7 +37,7 @@ function validateUsername(){
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
-  document.getElementById("usernameGroup").classList.remove("has-error");
+
   document.getElementById("passwordGroup").classList.add("has-success");
   }
 }
@@ -47,7 +47,7 @@ function validatePassword(){
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
     //Turn the password items red
-    document.getElementById("passwordGroup").classList.remove("has-success");
+
     document.getElementById("passwordGroup").classList.add("has-error");
   }
   else {
@@ -55,7 +55,7 @@ function validatePassword(){
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
-  document.getElementById("passwordGroup").classList.remove("has-error");
+
   document.getElementById("passwordGroup").classList.add("has-success");
   }
   if (passEntered.length>=10){
@@ -63,7 +63,7 @@ function validatePassword(){
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
     //Turn the password items red
-    document.getElementById("passwordGroup").classList.remove("has-success");
+
     document.getElementById("passwordGroup").classList.add("has-error");
   }
   else {
@@ -71,7 +71,7 @@ function validatePassword(){
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
-  document.getElementById("passwordGroup").classList.remove("has-error");
+
   document.getElementById("passwordGroup").classList.add("has-success");
   }
   if (passEntered==userEntered){
@@ -79,7 +79,7 @@ function validatePassword(){
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
     //Turn the password items red
-    document.getElementById("passwordGroup").classList.remove("has-success");
+
     document.getElementById("passwordGroup").classList.add("has-error");
   }
   else {
@@ -87,7 +87,7 @@ function validatePassword(){
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
-  document.getElementById("passwordGroup").classList.remove("has-error");
+
   document.getElementById("passwordGroup").classList.add("has-success");
 }
 }
